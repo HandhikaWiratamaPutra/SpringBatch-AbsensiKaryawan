4abe80cf88855ee4475165301a8b2bcd8df7f040
Handhika Wiratam Putra <yohaneshandhika98@gmail.com>
2024-06-03 22:03:27 +0700

    initial commit

---
+++ src/main/java/one/bca/SpringBatch_AbsensiKaryawan/SpringBatchAbsensiKaryawanApplication.java
@@ -0,0 +1,13 @@
+package one.bca.SpringBatch_AbsensiKaryawan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchAbsensiKaryawanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchAbsensiKaryawanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/bca/SpringBatch_AbsensiKaryawan/SpringBatchAbsensiKaryawanApplicationTests.java
@@ -0,0 +1,13 @@
+package one.bca.SpringBatch_AbsensiKaryawan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchAbsensiKaryawanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
