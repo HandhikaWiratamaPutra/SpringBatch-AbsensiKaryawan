2d5dc354b915d9cddb865a8c167e39e35a3a14c5
Handhika Wiratam Putra <yohaneshandhika98@gmail.com>
2024-06-04 20:32:33 +0700

    add Cuti to model

---
+++ src/main/java/one/bca/SpringBatch_AbsensiKaryawan/model/Cuti.java
@@ -0,0 +1,10 @@
+package one.bca.SpringBatch_AbsensiKaryawan.model;
+
+import java.util.Date;
+
+public class Cuti {
+    Long cutiId;
+    Long karyawanId;
+    Date tanggalCuti;
+    String alasanCuti;
+}
