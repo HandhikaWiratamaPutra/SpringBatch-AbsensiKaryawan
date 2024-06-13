07ba7406f3ff878ad205b3e33f568103cb0b61f3
Handhika Wiratam Putra <yohaneshandhika98@gmail.com>
2024-06-13 22:43:44 +0700

    add test class for step1 and stepAbsensiHarianToBulanan

--- src/test/java/one/bca/SpringBatch_AbsensiKaryawan/configuration/BatchConfigurationTest.java
+++ src/test/java/one/bca/SpringBatch_AbsensiKaryawan/configuration/BatchConfigurationTest.java
@@ -3,22 +3,13 @@ package one.bca.SpringBatch_AbsensiKaryawan.configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.batch.core.*;
-import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-//@Configuration
-//@ExtendWith(SpringExtension.class)
 @SpringBatchTest
 @SpringBootTest
 class BatchConfigurationTest {
@@ -30,12 +21,6 @@ class BatchConfigurationTest {
 
     private JobExecution jobExecution;
 
-    @AfterEach
-    void cleanup() {
-        // Clean up job executions after each test
-//        jobRepositoryTestUtils.removeJobExecutions();
-    }
-
     @Test
     void jobStart() throws Exception {
         //pastikan @Bean di job tidak di comment
@@ -43,7 +28,6 @@ class BatchConfigurationTest {
         JobParameters jobParameters = new JobParametersBuilder()
                 .addLong("time - testing", currentMilis)
                 .toJobParameters();
-//        jobLauncherTestUtils.setJob(job);
 
         //when Launch the job
         jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
@@ -59,10 +43,39 @@ class BatchConfigurationTest {
         JobParameters jobParameters = new JobParametersBuilder()
                 .addLong("time - testing", currentMilis)
                 .toJobParameters();
-//        jobLauncherTestUtils.setJob(job);
 
         //when Launch the job
         jobExecution = jobLauncherTestUtils.launchStep("masterCreateOutputCSVAbsensi", jobParameters);
+
+        //then
+        Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+    }
+
+    @Test
+    void step1() {
+        //pastikan @Bean di job tidak di comment
+        Long currentMilis = System.currentTimeMillis();
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("time - testing", currentMilis)
+                .toJobParameters();
+
+        //when Launch the job
+        jobExecution = jobLauncherTestUtils.launchStep("step1", jobParameters);
+
+        //then
+        Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+    }
+
+    @Test
+    void stepAbsensiHarianToBulanan() {
+        //pastikan @Bean di job tidak di comment
+        Long currentMilis = System.currentTimeMillis();
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("time - testing", currentMilis)
+                .toJobParameters();
+
+        //when Launch the job
+        jobExecution = jobLauncherTestUtils.launchStep("stepAbsensiHarianToBulanan", jobParameters);
 
         //then
         Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
